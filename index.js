f8f58139b9b3a5231867a9ec8fa9d4779e6aa062
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-08 17:11:15 +0100

    map and genreList

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
+const { string } = require("i/lib/util");
+
 // Definire un array di oggetti, ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv).
 let filmsAndSeries = [
     {
         title: 'Breaking Bad',
         year: 2008,
-        genre: 'Crime, Drama, Thriller',
+        genre: ['Crime', 'Drama', 'Thriller'],
         rating: 9.5,
-        type: 'tv',
+        type: 'Tv Series',
         seasons: 5
     },
     {
         title: 'Game of Thrones',
         year: 2011,
-        genre: 'Action, Adventure, Drama',
+        genre: ['Action', 'Adventure', 'Drama'],
         rating: 9.3,
-        type: 'tv',
+        type: 'Tv Series',
         seasons: 8
     },
     {
         title: 'Stranger Things',
         year: 2016,
-        genre: 'Drama, Fantasy, Horror',
+        genre: ['Drama', 'Fantasy', 'Horror'],
         rating: 8.7,
-        type: 'tv',
+        type: 'Tv Series',
         seasons: 4
     },
     {
         title: 'The Crown',
         year: 2016,
-        genre: 'Biography, Drama, History',
+        genre: ['Biography', 'Drama', 'History'],
         rating: 8.7,
-        type: 'tv',
+        type: 'Tv Series',
         seasons: 4
     },
     {
         title: 'Inception',
         year: 2010,
-        genre: 'Action, Adventure, Sci-Fi',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
         rating: 8.8,
         type: 'Movie'
     },
     {
         title: 'Interstellar',
         year: 2014,
-        genre: 'Adventure, Drama, Sci-Fi',
+        genre: ['Adventure', 'Drama', 'Sci-Fi'],
         rating: 8.6,
         type: 'Movie'
     }
@@ -77,10 +79,12 @@ class Movie{
 
 // METODI --------------------------------------------------------
 // Entrambe le classi dovranno avere un metodo toString() che ritorni una stringa con i dati del film, tipo: 
-// Breaking Bad è una serie tv di genere Drama. La prima stagione è stata rilasciato nel 2008 ed in totale sono state prodotte 5 stagioni. Ha un voto di 9.5
+
     toString() {
         return `${this.title} is a ${this.genre} ${this.type}. It was released in ${this.year} and has a rating of ${this.rating}`;
     }
+
+ 
 }
 
 let movie1 = new Movie("Talk to me", 2023,'Horror',6,'movie');
@@ -105,12 +109,43 @@ class Tv extends Movie{
 
     toString() {
         return `
-        ${this.title} is a ${this.genre} ${this.type} series. There are ${this.seasons} seasons realeased in ${this.year} . It has a grade of ${this.rating}`;
+        ${this.title} is a ${this.genre} ${this.type}. There are ${this.seasons} seasons realeased in ${this.year} . It has a grade of ${this.rating}`;
     }
 
 }
 
-let series1 = new Tv("Guardian: The Lonely and Great God", 2016,'Drama',9.4,'tv', 1);
+let series1 = new Tv("Guardian: The Lonely and Great God", 2016,'Drama',9.4,'Tv Series', 1);
+
+// Tramite la funzione .map(), creare un nuovo array dove per ogni elemento dell’array di oggetti viene creata un istanza della classe Movie o TvSerie in base al type e salvata nel nuovo array.
+const filmsAndSeriesTypes = filmsAndSeries.map((singleFilmAndSeries)=>{
+    return singleFilmAndSeries.type;
+});
+
+// Creiamo una funzione che restituisca la media dei voti di tutti i film per un determinato genere. Prevedere un argomento per la lista dei film ed uno per il genere.
+
+
+
+
+// Creiamo una funzione che restituisca la lista di tutti i generi dei film, senza che questi si ripetano.
+    // Inizializza un array vuoto per memorizzare i generi univoci
+    let genreList = [];
+
+    // ciclo ogni oggetto nell'array
+    filmsAndSeries.forEach((el) => {
+        // ciclo i generi di ogni oggetto
+        el.genre.forEach((genre) => {
+            // se il genere non è già presente nell'array dei generi
+            if (!genreList.includes(genre)) {
+                // lo aggiungo a genreList
+                genreList.push(genre);
+            }
+        });
+    });
+
+
+
+
+
 
 console.log(movie1);
 
@@ -120,5 +155,6 @@ console.log(series1);
 
 console.log(series1.toString());
 
+console.log(filmsAndSeriesTypes);
 
-
+console.log(genreList);
